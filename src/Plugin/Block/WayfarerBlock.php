ce6812c347ed9e5c78c2bfb35a69e57c4287fde5
Tara King <tara.king@umusic.com>
2018-06-06 21:36:29 -0700

    Cleaning up some namespacing and simplfying the display array.

--- src/Plugin/Block/WayfarerBlock.php
+++ src/Plugin/Block/WayfarerBlock.php
@@ -16,7 +16,7 @@ use Drupal\Core\Url;
  *   admin_label = @Translation("Travel Alert"),
  * )
  */
-class wayfarerBlock extends BlockBase implements BlockPluginInterface {
+class WayfarerBlock extends BlockBase implements BlockPluginInterface {
 
   /**
    * {@inheritdoc}
@@ -25,24 +25,24 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     $config = $this->getConfiguration();
 
     // Get the travel destination from Drupal config and set a reasonable default.
-    if (!empty($config['place'])) {
-      $place = $config['place'];
+    if (!empty($config['wayfarer_place'])) {
+      $place = $config['wayfarer_place'];
     }
     else {
       $place = $this->t('my home office.');
     }
 
     // Get the arrival date from Drupal config and set a reasonable default.
-    if (!empty($config['date1'])) {
-      $date1 = $config['date1'];
+    if (!empty($config['wayfarer_date1'])) {
+      $date1 = $config['wayfarer_date1'];
     }
     else {
       $date1 = $this->t('now');
     }
 
     // Get the departure date from Drupal config and set a reasonable default.
-    if (!empty($config['date2'])) {
-      $date2 = $config['date2'];
+    if (!empty($config['wayfarer_date2'])) {
+      $date2 = $config['wayfarer_date2'];
     }
     else {
       $date2 = $this->t('the foreseeable future');
@@ -50,38 +50,26 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     // @todo Display dates in more human-readable form than YYYY-MM-DD.  
 
     // Get schedule link from Drupal config and set a reasonable default.
-    if (!empty($config['schedule_link'])) {
-      $schedule_link = $config['schedule_link'];
+    $external_link = $this->t('Contact me');
+    if (!empty($config['wayfarer_schedule_link'])) {
+      $schedule_link = $config['wayfarer_schedule_link'];
       $url = Url::fromUri($schedule_link);
-      $external_link = \Drupal\Core\Link::fromTextAndUrl('here', $url)->toString();
-     
-      // Build block display when there is a schedule link available.     
-      $build = array(
-        '#markup' => $this->t(
-          'I\'m going to be in @place from @date1 until @date2. Click @external_link to schedule a face-to-face meeting while I\'m in town.', 
-          array (
-            '@place' => $place,
-            '@date1' => $date1,
-            '@date2' => $date2,
-            '@external_link' => $external_link, 
-          )
+      $external_link = $this->t('Click ') . \Drupal\Core\Link::fromTextAndUrl('here', $url)->toString();
+     }
+
+    // Build block display.
+    $build = array(
+      '#markup' => $this->t(
+        'I\'m going to be in @place from @date1 until @date2. @external_link to schedule a face-to-face meeting while I\'m in town.',
+        array (
+          '@place' => $place,
+          '@date1' => $date1,
+          '@date2' => $date2,
+          '@external_link' => $external_link,
         )
-      );
-    }
-    
-    // Build block display when there is no schedule link available. 
-    else {
-      $build = array(
-        '#markup' => $this->t(
-          'I\'m going to be in @place from @date1 until @date2. Contact me to schedule a face-to-face meeting while I\'m in town.', 
-          array (
-            '@place' => $place,
-            '@date1' => $date1,
-            '@date2' =>$date2,
-          )
-        )
-      );
-    }   
+      )
+    );
+
     // @todo Add link to contact form for users without a scheduling system.
    
     return $build;
@@ -99,7 +87,7 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     $form['wayfarer_block_place'] = array (
       '#type' => 'textfield',
       '#title' => $this->t('Where'),
-      '#default_value' => isset($config['place']) ? $config['place'] : '',
+      '#default_value' => isset($config['wayfarer_place']) ? $config['wayfarer_place'] : '',
       '#description' => $this->t('Where are you traveling?'),
       '#required' => TRUE,
     );
@@ -108,7 +96,7 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     $form['wayfarer_block_date1'] = array (
       '#type' => 'date',
       '#title' => $this->t('Arrival date'),
-      '#default_value' => isset($config['date1']) ? $config['date1'] : '',
+      '#default_value' => isset($config['wayfarer_date1']) ? $config['wayfarer_date1'] : '',
       '#description' => 'When are you arriving?',
       '#required' => TRUE,
 
@@ -118,8 +106,8 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     $form['wayfarer_block_date2'] = array (
       '#type' => 'date',
       '#title' => $this->t('Departure date'),
-      '#default_value' => isset($config['date2']) ? $config['date2'] : '',
-      '#description' => 'When are you leaving?',
+      '#default_value' => isset($config['wayfarer_date2']) ? $config['wayfarer_date2'] : '',
+      '#description' => $this->t('When are you leaving?'),
       '#required' => TRUE,
     );
     // @todo Expire/hide the block when date2 has passed.
@@ -127,7 +115,7 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
     $form['wayfarer_block_schedule_link'] = array (
       '#type' => 'textfield',
       '#title' => $this->t('Schedule link'),
-      '#default_value' => isset($config['schedule_link']) ? $config['schedule_link'] : '',
+      '#default_value' => isset($config['wayfarer_schedule_link']) ? $config['wayfarer_schedule_link'] : '',
       '#description' => $this->t('Put in a link to the scheduling service you use.'),
     );    
     return $form;
@@ -137,7 +125,7 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
    * {@inheritdoc}
    */
   public function blockValidate($form, FormStateInterface $form_state) {
-    // Check is date2 is after date1.
+    // Check if date2 is after date1.
     $date1 = $form_state->getValue('wayfarer_block_date1');
     $date2 = $form_state->getValue('wayfarer_block_date2');
     if ($date2 < $date1) {
@@ -149,9 +137,9 @@ class wayfarerBlock extends BlockBase implements BlockPluginInterface {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->setConfigurationValue('place', $form_state->getValue('wayfarer_block_place'));
-    $this->setConfigurationValue('date1', $form_state->getValue('wayfarer_block_date1'));
-    $this->setConfigurationValue('date2', $form_state->getValue('wayfarer_block_date2'));
-    $this->setConfigurationValue('schedule_link', $form_state->getValue('wayfarer_block_schedule_link'));
+    $this->setConfigurationValue('wayfarer_place', $form_state->getValue('wayfarer_block_place'));
+    $this->setConfigurationValue('wayfarer_date1', $form_state->getValue('wayfarer_block_date1'));
+    $this->setConfigurationValue('wayfarer_date2', $form_state->getValue('wayfarer_block_date2'));
+    $this->setConfigurationValue('wayfarer_schedule_link', $form_state->getValue('wayfarer_block_schedule_link'));
   }
 }
